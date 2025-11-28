a315e8111b11cd5396f3ef06b9db605513695171
Hugo Philipp <hphilipp@scaleway.com>
2025-11-28 16:31:26 +0100

    feat: Containerize frontend and backend services using Docker Compose.

--- backend/fastapi_app/config.py
+++ backend/fastapi_app/config.py
@@ -2,7 +2,7 @@ import os
 
 # Get the current directory (fastapi_app)
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-# Parent directory (project root)
+# Parent directory (backend root)
 PARENT_DIR = os.path.dirname(CURRENT_DIR)
 
 # Paths
